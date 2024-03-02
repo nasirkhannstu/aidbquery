77577532adddee60ac34a33f85e7f5480961dfb8
Hozaifa Bin Masud <hozaifa4you@gmail.com>
2024-03-02 18:44:59 +0600

    remove unecessery dependency

---
+++ src/lib/errors.ts
@@ -0,0 +1,8 @@
+export const ERRORS = {
+  invalidEmail: {
+    reason: "Invalid email",
+    message: "The email you have entered is invalid",
+    codeStr: "BAD_REQUEST",
+    code: 400,
+  },
+};
