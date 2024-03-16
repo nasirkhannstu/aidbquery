ee023220bca983ad3d7f0c3ed0b1b44225b96c93
Hozaifa Bin Masud <hozaifa4you@gmail.com>
2024-03-16 09:46:45 +0600

    refactor some previous code

--- trpc/server/routers/messages.ts
+++ trpc/server/routers/messages.ts
@@ -9,7 +9,7 @@ import { INFINITY_QUERY } from "@/lib/utils";
 import { fileErrors } from "@/lib/alerts/errors.trpc";
 
 export const messageRouter = createTRPCRouter({
-  messagesOfFile: protectedProcedure
+  fileMessages: protectedProcedure
     .input(
       z.object({
         limit: z.number().min(1).max(100).default(INFINITY_QUERY),
@@ -32,24 +32,20 @@ export const messageRouter = createTRPCRouter({
           message: fileErrors.fileNotFound.message,
         });
 
-      const msgs = await ctx.db.query.messages.findMany(
+      const prevMessages = await ctx.db.query.messages.findMany(
         withCursorPagination({
           where: eq(messages.fileId, input.fileId),
           limit: input.limit,
           cursors: [
-            [
-              messages.createdAt,
-              "asc",
-              input.cursor ? new Date(input.cursor) : undefined,
-            ],
+            [messages.id, "asc", input.cursor ? input.cursor : undefined],
           ],
         }),
       );
 
       return {
-        messages: msgs,
-        nextCursor: msgs.length
-          ? msgs[msgs.length - 1]?.createdAt.toISOString()
+        messages: prevMessages,
+        nextCursor: prevMessages.length
+          ? prevMessages[prevMessages.length - 1]?.id
           : null,
       };
     }),
